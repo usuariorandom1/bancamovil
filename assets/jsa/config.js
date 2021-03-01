0fa9c7e5d72d615695df233f766a4f1f858d27d2
carlosmr <carlosmr@users.noreply.github.com>
2021-03-01 18:39:34 -0500

    1

--- assets/jsa/config.js
+++ assets/jsa/config.js
@@ -1,5 +1,5 @@
 var metacoinConfig = {
-  "contractAddress": "TRdZdgADqLPdux2urgriUe2Pjhbmuh2Nsv",
+  "contractAddress": "TRA5xDpiCtB4KTWobaWxtnEmeqWYBsEjiN",
   "privateKey": "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0",
   "fullHost": "https://api.trongrid.io"
 }
