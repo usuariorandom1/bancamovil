06eb7f0dc2c973381907e991ecd85d4768dbfac5
usuariorandom1 <edereco@MacBook-Pro-de-Eduardo.local>
2021-03-01 23:21:15 -0500

    1

--- assets/jsa/config.js
+++ assets/jsa/config.js
@@ -1,5 +1,5 @@
 var metacoinConfig = {
-  "contractAddress": "TRA5xDpiCtB4KTWobaWxtnEmeqWYBsEjiN",
+  "contractAddress": "TMQjGYV2Po2Ejd92VsPpi7KQ1RzrKqPVQE",
   "privateKey": "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0",
   "fullHost": "https://api.trongrid.io"
 }
