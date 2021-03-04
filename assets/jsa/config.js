8517f422ef8a60bf03bf257b5b68f030a4820654
usuariorandom1 <72815503+usuariorandom11@users.noreply.github.com>
2021-03-03 22:50:43 -0500

    Update config.js

--- assets/jsa/config.js
+++ assets/jsa/config.js
@@ -1,5 +1,5 @@
 var metacoinConfig = {
-  "contractAddress": "",
+  "contractAddress": "TEJXLrsGKZ1wfJZ1XiDBfcBNxsKRMCG5BN",
   "privateKey": "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0",
   "fullHost": "https://api.trongrid.io"
 }
